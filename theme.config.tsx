400e0a271beb6ac59b0c9434673751acb6ee05d1
hellskater <ksrinivasrao531@gmail.com>
2023-02-12 17:18:31 +0530

    formatted folders

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>NarutoDB</span>,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
-  },
-  chat: {
-    link: "https://discord.com",
+    link: "https://github.com/hellskater/narutodb-docs",
   },
   docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: "Nextra Docs Template",
+    text: "NarutoDB",
   },
 };
 
