804590e6511d568e65636bc0973499894b983951
hellskater <ksrinivasrao531@gmail.com>
2023-02-26 20:13:01 +0530

    title bug

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,7 +13,7 @@ const config: DocsThemeConfig = {
   primaryHue: 29,
   useNextSeoProps() {
     return {
-      defaultTitle: "NarutoDB",
+      // defaultTitle: "NarutoDB",
       titleTemplate: "%s | NarutoDB",
       description:
         "NarutoDB API provides a comprehensive database of all the Characters, Clans, Villages, Kekkeigenkai, Tailed Beasts, Teams, Akatsuki, and Kara members from the most awesome anime and manga series, Naruto. With our API, you can easily access detailed information about each character, including their name, appearance, personality, jutsus and Ninja tools. Whether you're a fan of the series, a developer building a Naruto-themed app, or just looking for fun trivia about your favorite characters, our API has you covered. Our API is designed to be easy to use and accessible to everyone, so you can start exploring the world of Naruto characters today.",
