9fb75432382ab6321b68e2a38ed643ec39c9c84c
hellskater <ksrinivasrao531@gmail.com>
2023-02-26 15:27:38 +0530

    added banner and font

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <span className="text-orange-500 font-bold text-xl font-naruto tracking-wider">
+      Naruto DB
+    </span>
+  );
+};
+
+export default Logo;
