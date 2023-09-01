7beba0b8d6ee6791229957d6b4f2890a5903c845
K Srinivas Rao <ksrinivasrao531@gmail.com>
2023-09-01 21:18:21 +0530

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 // minor change
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 }
