ea78dab561beafd1c031388d621eb51759b82e75
K Srinivas Rao <ksrinivasrao531@gmail.com>
2023-11-27 21:00:03 +0530

    cors fix

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const config = {
 				headers: [
 					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
 					{ key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
-					{ key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+					{ key: 'Access-Control-Allow-Methods', value: 'GET' },
 					{
 						key: 'Access-Control-Allow-Headers',
 						value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
